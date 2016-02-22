26dfa4b8ed850ebcb4cfdb1df758d93be1de2bbd
Wilmer <Wilmer@Gohan.local>
2016-02-21 22:01:14 -0800

    adding files

---
+++ assignments/jsjqas03.js
@@ -0,0 +1,99 @@
+    //**********************************************************
+    // Code your JavaScript for the assignment
+    //
+    // The following comments show the suggested order of
+    // your code. You will need to write the code under each
+    // comment to perform the operations described by the
+    // comment.
+    //
+    // You can alter the suggested order. You are free to
+    // write the code in whatever order you want, so long
+    // as you accomplish the goals of the assignment.
+    //**********************************************************
+
+
+
+    //**********************************************************
+    // Step 1: define a function that will be used to
+    //         work with the values that are entered,
+    //         validate the values, calculate the results,
+    //         and display the results
+    //
+    // NOTE: you may enter all of the code in one function
+    //       or you may split the code into as many functions
+    //       as you want.
+    //**********************************************************
+
+
+
+    //**********************************************************
+    // Step 2: retrieve the values from the input type=text
+    //         form elements, assign values to variables
+    //**********************************************************
+
+
+
+    //**********************************************************
+    // Step 3: check for valid miles driven
+    //         if (not a number OR miles driven < 0),
+    //         write an error message to the form
+    //         using the miles_driven_error id
+    //**********************************************************
+
+
+
+    //**********************************************************
+    // Step 4: check for valid gallons used
+    //         if (not a number OR gallons used < 0),
+    //         write an error message to the form
+    //         using the gallons_used_error id
+    //**********************************************************
+
+
+
+    //**********************************************************
+    // Step 5: check for valid price per gallons
+    //         if (not a number OR price per gallon < 0)
+    //         write an error message to the form
+    //         using the price_gallons_error id
+    //**********************************************************
+
+
+
+    //**********************************************************
+    // Step 6: perform calculations
+    //         if the three factors are valid, calculate
+    //         the miles per gallon and the cost of the trip
+    //**********************************************************
+
+
+
+    //**********************************************************
+    // Step 7: write values to the page
+    //         write the results of the calculated values from
+    //         Step 6 to the page, using the mpg and cost id's.
+    //         Include text that identifies the value that
+    //         you are writing to the page.
+    //**********************************************************
+
+
+
+    //**********************************************************
+    // Step 8: define a function to clear the page
+    //         This function will be invoked when the
+    //         "Clear Form" button is clicked.
+    //
+    //         Clear the values in the text boxes and the
+    //         mpg and cost id's that are used to display
+    //         the results.
+    //**********************************************************
+
+
+
+    //**********************************************************
+    // Step 9: define an anonymous function for window.onload
+    //         This function should assign the event handler
+    //         functions for the Calculate and Clear buttons
+    //**********************************************************
+
+
