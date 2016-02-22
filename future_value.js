c92e2581ce261021a341d5f56ff983d6fb4f39af
Wilmer <Wilmer@Gohan.local>
2016-02-21 21:13:36 -0800

    future value js

--- future_value.js
+++ future_value.js
@@ -1,3 +1,26 @@
 var $ = function (id) {
     return document.getElementById(id);
-}
+};
+
+var calculateClick = function() {
+    var investmentAmount =  parseFloat($('investment').value);
+    var taxRate = parseFloat($('rate').value);
+    var numOfYears = parseInt($('years').value);
+    var futureValue = investmentAmount;
+
+
+    for(i = 1; i <= numOfYears; i++){
+
+        futureValue += futureValue * taxRate /100;
+
+    }
+
+    $('future_value').value = futureValue.toFixed(0);
+
+};
+
+
+window.onload = function() {
+  $('calculate').onclick = calculateClick;
+};
+
