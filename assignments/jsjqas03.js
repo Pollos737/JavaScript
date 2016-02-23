1f37128b333e80e7820bf8c4d085986bb0455260
Wilmer <Wilmer@iMac-2.local>
2016-02-23 09:57:37 -0800

    fix merge conflict from hell

--- assignments/jsjqas03.js
+++ assignments/jsjqas03.js
@@ -1,33 +1,5 @@
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 //Wilmer Guerra
 
-=======
- //**********************************************************
-    // Step 1: define a function that will be used to
-    //         work with the values that are entered,
-    //         validate the values, calculate the results,
-    //         and display the results
-    //
-    // NOTE: you may enter all of the code in one function
-    //       or you may split the code into as many functions
-    //       as you want.
-    //**********************************************************
->>>>>>> eff066280ee530c67a586958729ccc982d590e47
-=======
-//Wilmer Guerra
-
->>>>>>> clear-button
-=======
-//Wilmer Guerra
-
->>>>>>> clear-button
-=======
-//Wilmer Guerra
-
->>>>>>> clear-button
  var $ = function(id) {
         return document.getElementById(id);
  };
@@ -48,43 +20,11 @@
          $('price_gallon_error').firstChild.nodeValue = '';
      }
      
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
      //important: validate value before using parseFloat.
      var miles = $('miles_driven').value;
      var gallons = $('gallons_used').value;
      var pricePerGallon = $('price_gallon').value;
      var mpg = parseFloat(miles / gallons).toFixed(2);
-=======
-     //TODO: don't parse userInputs before validation.
-     var miles = $('miles_driven').value;
-     var gallons = $('gallons_used').value;
-     var pricePerGallon = $('price_gallon').value;
-     var mpg = parseFloat(miles * gallons).toFixed(2);
->>>>>>> eff066280ee530c67a586958729ccc982d590e47
-=======
-     //important: validate value before using parseFloat.
-     var miles = $('miles_driven').value;
-     var gallons = $('gallons_used').value;
-     var pricePerGallon = $('price_gallon').value;
-     var mpg = parseFloat(miles / gallons).toFixed(2);
->>>>>>> clear-button
-=======
-     //important: validate value before using parseFloat.
-     var miles = $('miles_driven').value;
-     var gallons = $('gallons_used').value;
-     var pricePerGallon = $('price_gallon').value;
-     var mpg = parseFloat(miles / gallons).toFixed(2);
->>>>>>> clear-button
-=======
-     //important: validate value before using parseFloat.
-     var miles = $('miles_driven').value;
-     var gallons = $('gallons_used').value;
-     var pricePerGallon = $('price_gallon').value;
-     var mpg = parseFloat(miles / gallons).toFixed(2);
->>>>>>> clear-button
      var costOfTrip = parseFloat(gallons * pricePerGallon).toFixed(2);
 
      var errorFound = "n";
@@ -110,23 +50,7 @@
      }
      
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
      //if error found, then nothing, if all good calculate and output results to user
-=======
-     //if error found, then nothing, if all good calculate and output restults to user
->>>>>>> eff066280ee530c67a586958729ccc982d590e47
-=======
-     //if error found, then nothing, if all good calculate and output results to user
->>>>>>> clear-button
-=======
-     //if error found, then nothing, if all good calculate and output results to user
->>>>>>> clear-button
-=======
-     //if error found, then nothing, if all good calculate and output results to user
->>>>>>> clear-button
      if(errorFound == "y"){
          //output "fix your stuff" to user
          $('mpg').firstChild.nodeValue = "Please fix the above errors and re-calculate"
@@ -141,36 +65,11 @@
  };
 
  var clearForm = function() {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
      //setting input fields to empty string
-=======
->>>>>>> eff066280ee530c67a586958729ccc982d590e47
-=======
-     //setting input fields to empty string
->>>>>>> clear-button
-=======
-     //setting input fields to empty string
->>>>>>> clear-button
-=======
-     //setting input fields to empty string
->>>>>>> clear-button
      $('miles_driven').value = "";
      $('gallons_used').value = "";
      $('price_gallon').value = "";
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> clear-button
-=======
->>>>>>> clear-button
-=======
->>>>>>> clear-button
      //if statements below check for error messages and clears them
      if($('miles_driven_error').value !== '')
      {
@@ -193,17 +92,6 @@
          $('cost').firstChild.nodeValue = ""
      }
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> eff066280ee530c67a586958729ccc982d590e47
-=======
->>>>>>> clear-button
-=======
->>>>>>> clear-button
-=======
->>>>>>> clear-button
  };
 
  window.onload = function() {
@@ -211,90 +99,6 @@
      $('btnClear').onclick = clearForm;
  };
 
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
 
-=======
-    //**********************************************************
-    // Step 2: retrieve the values from the input type=text
-    //         form elements, assign values to variables
-    //**********************************************************
-
-
-
-    //**********************************************************
-    // Step 3: check for valid miles driven
-    //         if (not a number OR miles driven < 0),
-    //         write an error message to the form
-    //         using the miles_driven_error id
-    //**********************************************************
-
-
-
-    //**********************************************************
-    // Step 4: check for valid gallons used
-    //         if (not a number OR gallons used < 0),
-    //         write an error message to the form
-    //         using the gallons_used_error id
-    //**********************************************************
-
-
-
-    //**********************************************************
-    // Step 5: check for valid price per gallons
-    //         if (not a number OR price per gallon < 0)
-    //         write an error message to the form
-    //         using the price_gallons_error id
-    //**********************************************************
-
-
-
-    //**********************************************************
-    // Step 6: perform calculations
-    //         if the three factors are valid, calculate
-    //         the miles per gallon and the cost of the trip
-    //**********************************************************
-
-
-
-    //**********************************************************
-    // Step 7: write values to the page
-    //         write the results of the calculated values from
-    //         Step 6 to the page, using the mpg and cost id's.
-    //         Include text that identifies the value that
-    //         you are writing to the page.
-    //**********************************************************
-
-
-
-    //**********************************************************
-    // Step 8: define a function to clear the page
-    //         This function will be invoked when the
-    //         "Clear Form" button is clicked.
-    //
-    //         Clear the values in the text boxes and the
-    //         mpg and cost id's that are used to display
-    //         the results.
-    //**********************************************************
-
-
-
-    //**********************************************************
-    // Step 9: define an anonymous function for window.onload
-    //         This function should assign the event handler
-    //         functions for the Calculate and Clear buttons
-    //**********************************************************
->>>>>>> eff066280ee530c67a586958729ccc982d590e47
-=======
-
->>>>>>> clear-button
-=======
-
->>>>>>> clear-button
-=======
-
->>>>>>> clear-button
 
 
